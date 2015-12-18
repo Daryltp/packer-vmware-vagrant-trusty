a7a8791d0dbeefcf964d03f29606f20f8e5128d0
Daryl Porter <d.porter@kainos.com>
2015-12-18 15:20:15 +0000

    adding carrenza upload functionality

--- buildVMware.sh
+++ buildVMware.sh
@@ -2,11 +2,13 @@ echo "Compressing current OVF file..."
 zip -r OVF/Ubuntu-1404lts-golden-OVF-$(date +"%m-%d-%y:%H:%M").zip OVF/ -x /OVF/*.zip
 find OVF/* -type f -not -name '*zip' | xargs rm
 echo "Setting Environment Variables..."
-export packervmname="ubuntu-1404lts-golden-$(date +"%m-%d-%y:%H%M")"
+export packervmname="ubuntu-1404lts-golden-$(date +"%m-%d-%y-%H%M")"
 echo "Running Packer..."
 packer build -force templates/ubuntu-1404-vmware.json
 echo "Converting Packer output to Carrenza compatible OVF file..."
 ovftool output-vmware-iso/$packervmname.vmx OVF/$packervmname.ovf
+echo "Uploading to Carrenza..."
+ovftool "OVF/$packervmname.ovf" "vcloud://:@myvdc.carrenza.net:443?org=hy0z&vdc=IER-VDC01&catalog=Packer&vappTemplate=$packervmname"
 echo "Tidying up Environment Variables..."
 unset packervmname
 echo "Finished"
