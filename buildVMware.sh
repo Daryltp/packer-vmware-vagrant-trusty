a5e2040bb4b476d293810dbcb2c9d372486b1dc0
Daryl Porter <d.porter@kainos.com>
2015-12-18 15:39:32 +0000

    date formatting

--- buildVMware.sh
+++ buildVMware.sh
@@ -2,7 +2,7 @@ echo "Compressing current OVF file..."
 zip -r OVF/Ubuntu-1404lts-golden-OVF-$(date +"%m-%d-%y:%H:%M").zip OVF/ -x /OVF/*.zip
 find OVF/* -type f -not -name '*zip' | xargs rm
 echo "Setting Environment Variables..."
-export packervmname="ubuntu-1404lts-golden-$(date +"%m-%d-%y-%H%M")"
+export packervmname="ubuntu-1404lts-golden-$(date +"%y-%m-%d-%H%M")"
 echo "Running Packer..."
 packer build -force templates/ubuntu-1404-vmware.json
 echo "Converting Packer output to Carrenza compatible OVF file..."
